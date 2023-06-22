ce2b1722c87fd8c4e852de73b9059bfbbff2c8bc
Leon Waltert <leon.waltert@micronova.de>
2023-06-22 11:40:21 +0200

    gonna try some shit

--- ApexTournamentThingy/MainWindow.xaml.cs
+++ ApexTournamentThingy/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace ApexTournamentThingy
 {
@@ -21,28 +22,52 @@ namespace ApexTournamentThingy
     /// </summary>
     public partial class MainWindow : Window
     {
+        Session session;
+
         public MainWindow()
         {
             InitializeComponent();
+            session = new Session(1);
         }
 
         private void team1namebox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Trace.WriteLine("Text changed");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            /*Session data = new Session();
+            data.id = 0;
+            Team team = new Team();
+            Player player = new Player();
+            player.id = 1;
+            player.name = "Profpyrus";
+            player.kills = 2;
+            player.deaths = 3;
+            team.players.Add(player);
+            team.id = 15;
+            team.name = "Team Bromance";
+            team.wins = 1;
+            data.teams.Add(team);
+            Trace.WriteLine(JsonConvert.SerializeObject(data));*/
+
+            int playerCount = 0;
+            teamTabControl.ApplyTemplate();
             foreach (TabItem tab in teamTabControl.Items)
             {
-                Grid item = LogicalTreeHelper.GetChildren(tab).OfType<Grid>().FirstOrDefault();
-                List<TextBox> team1stuff = item.Children.OfType<TextBox>().ToList();
-                foreach (TextBox box in team1stuff)
+                Team team = new Team(tab.TabIndex, tab.GetTextboxValueByName("namebox"));
+                for (int i = 1; i <= 3; i++)
                 {
-                    Trace.WriteLine(box.Text);
+                    string name = tab.GetTextboxValueByName("player" + i + "box");
+                    if (name != "")
+                    {
+                        team.players.Add(new Player(playerCount, name));
+                        playerCount++;
+                    }
                 }
+                session.teams.Add(team);
             }
-            Trace.WriteLine("Test console output");
+            Trace.WriteLine(JsonConvert.SerializeObject(session));
         }
     }
 }
