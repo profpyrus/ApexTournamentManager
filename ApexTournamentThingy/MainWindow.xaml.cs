08cb140f7b5b99345e4307d6e89523fcf9ac61d8
Leon Waltert <leon.waltert@micronova.de>
2023-06-22 15:28:10 +0200

    got some stuff to work ab jetzt ordentlich commit messages bitte danke

--- ApexTournamentThingy/MainWindow.xaml.cs
+++ ApexTournamentThingy/MainWindow.xaml.cs
@@ -57,13 +57,37 @@ namespace ApexTournamentThingy
             Trace.WriteLine(JsonConvert.SerializeObject(data));*/
 
             session.teams.Add(new Team(5, "Looser's Club"));
-            session.teams[0].players.Add(new Player(3, "AnTique"));
+            ((Team)session.teams[0]).players.Add(new Player(3, "AnTique"));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Trace.WriteLine(JsonConvert.SerializeObject(session));
         }
+
+        private void AddTeam(object sender, RoutedEventArgs e)
+        {
+            int newTeamId = session.teams.Count + 1;
+            Team newTeam = new Team(newTeamId, "Team " + newTeamId);
+            session.teams.Add(newTeam);
+        }
+
+        private void RemoveTeam(object sender, RoutedEventArgs e)
+        {
+            Object selectedItem = teamTabControl.SelectedItem as TabItem;
+            //Trace.WriteLine(selectedItem.Header);
+        }
+        private void AddPlayer(object sender, RoutedEventArgs e)
+        {
+            int teamId = Convert.ToInt32(((Button)sender).Tag.ToString());
+            ((Team)session.teams[teamId-1]).players.Add(new Player(1, "New Player"));
+        }
+
+        private void RemovePlayer(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void TextBox_KeyEnterUpdate(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
