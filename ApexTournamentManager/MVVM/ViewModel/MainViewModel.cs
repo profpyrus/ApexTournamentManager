9f7533ef09ed560a839b6cba7d7987ab4c285eb1
Leon Waltert <leon.waltert@micronova.de>
2023-06-26 11:24:54 +0200

    add mvvm structure files

---
+++ ApexTournamentManager/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ApexTournamentManager.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexTournamentManager.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand TeamViewCommand { get; set; }
+        public RelayCommand MatchViewCommand { get; set; }
+
+        public TeamManagementViewModel TeamVM { get; set; }
+
+        public MatchManagementViewModel MatchVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            TeamVM = new TeamManagementViewModel();
+            MatchVM = new MatchManagementViewModel();
+            CurrentView = TeamVM;
+
+            TeamViewCommand = new RelayCommand(o => { CurrentView = TeamVM; });
+            MatchViewCommand = new RelayCommand(o => { CurrentView = MatchVM; });
+        }
+    }
+}
